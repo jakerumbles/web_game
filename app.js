4191434e99b56d8b988e25efcb3caec35eb9e04d
Jake Edwards <edwardsjake@live.com>
2018-08-05 14:23:01 -0600

    Finally got it to recognize the JS file
    <script type="text/javascript" src="/javascripts/game.js"></script>
    
    app.use(express.static(path.join(__dirname, "public")));

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require("express");
+var path = require("path");
 var app = express();
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
 app.get("/", function(req, res) {
-   res.send("Hello World");
+   res.render("index");
 });
 
 app.listen(3000, function() {
